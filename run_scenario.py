8c0a05315e7b71f1b80de9fc7507de53e7cf4f9e
Christiano Machado <christianomachado10@gmail.com>
2020-09-24 16:17:56 -0300

    feat: runner renamed

--- run_scenario.py
+++ run_scenario.py
@@ -1,9 +1,6 @@
 from mininet.log import setLogLevel
 from topology import scenario
 
-import sys
-
-# TODO: mudar o nome do arquivo e permitir que a escolha entre duas simulações
 
 if __name__ == '__main__':
     setLogLevel('info')

f67c7a2ddacd3c10157366eb3c505afc5185b9e6
Christiano Machado <christianomachado10@gmail.com>
2020-09-26 22:50:57 -0300

    feat: rsu and vehicle agent extends to an agent

---
+++ network_agent/rsu_agent.py
@@ -0,0 +1,28 @@
+import scapy.all
+import time
+import logging
+import os
+
+from scapy.all import conf
+from scapy.all import AsyncSniffer, send
+from scapy.layers.inet import Ether, IP, ICMP
+from time import sleep
+from random import randint
+from threading import Thread
+from enum import IntEnum
+from network_log.logger import Logging
+from utils.subnetutils import SubnetUtils
+from .default_agent import Agent
+
+
+class RSUAgent(Agent):
+
+    def __init__(self, name, **kwargs):
+        super(RSUAgent, self).__init__(name, **kwargs)
+        self.AGENT_CODE = 0x11  # 17
+
+    def response(self, packet):
+        pass
+
+    def request(self):
+        pass

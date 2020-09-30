f7b9ce0d3723d3fe496d1052afed0b2b01333e6a
Christiano Machado <christianomachado10@gmail.com>
2020-09-30 17:19:10 -0300

    fix: relative path

--- run_controller.py
+++ run_controller.py
@@ -1,13 +1,18 @@
 from network_controller.poxcontroller import PoxController
+import os
+
+ABS_PATH = os.path.abspath('')
+POX_PATH = os.path.abspath(f'../pox')
+LOG_NAME = 'pox'
 
 if __name__ == '__main__':
-    pox = PoxController(cmd='/home/ubuntu/pox/pox.py',
+    pox = PoxController(cmd=f'{POX_PATH}/pox.py',
                         script='forwarding.l3_learning',
                         ofst='openflow.spanning_tree --no-flood --hold-down',
                         debug='log.level --DEBUG',
                         pretty_log='samples.pretty_log',
                         ofdc='openflow.discovery host_tracker info.packet_dump',
                         ofport='openflow.of_01 --port=6653',
-                        log='log --file=/home/ubuntu/SDVANETS/log_files/pox.log'
+                        log=f'log --file={ABS_PATH}/log_files/{LOG_NAME}.log'
                         )
     pox.run()
